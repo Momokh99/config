7b3512586b6a48ae47f4df496c3e855199f5d61e
Momokh99 <mohamedkhamekhami@gmail.com>
2026-02-19 15:04:10 +0000

    Add files via upload

---
+++ kde/my theme/contents/layouts/org.kde.plasma.desktop-layout.js
@@ -0,0 +1,100 @@
+var plasma = getApiVersion(1);
+
+var layout = {
+    "desktops": [
+        {
+            "applets": [
+            ],
+            "config": {
+                "/": {
+                    "ItemGeometries-1536x864": "",
+                    "ItemGeometriesHorizontal": "",
+                    "formfactor": "0",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                },
+                "/Wallpaper/org.kde.image/General": {
+                    "Image": "file:///usr/share/wallpapers/ScarletTree/",
+                    "SlidePaths": "/home/mohamed/.local/share/wallpapers/,/usr/share/wallpapers/"
+                }
+            },
+            "wallpaperPlugin": "org.kde.image"
+        }
+    ],
+    "panels": [
+        {
+            "alignment": "center",
+            "applets": [
+                {
+                    "config": {
+                        "/": {
+                            "PreloadWeight": "100",
+                            "popupHeight": "697",
+                            "popupWidth": "647"
+                        },
+                        "/General": {
+                            "favoritesPortedToKAstats": "true"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.kickoff"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.pager"
+                },
+                {
+                    "config": {
+                        "/General": {
+                            "launchers": "applications:systemsettings.desktop,applications:org.kde.discover.desktop,preferred://filemanager,preferred://browser,file:///var/lib/flatpak/app/app.zen_browser.zen/current/active/export/share/applications/app.zen_browser.zen.desktop"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.icontasks"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.marginsseparator"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.systemtray"
+                },
+                {
+                    "config": {
+                        "/": {
+                            "popupHeight": "400",
+                            "popupWidth": "560"
+                        }
+                    },
+                    "plugin": "org.kde.plasma.digitalclock"
+                },
+                {
+                    "config": {
+                    },
+                    "plugin": "org.kde.plasma.showdesktop"
+                }
+            ],
+            "config": {
+                "/": {
+                    "formfactor": "2",
+                    "immutability": "1",
+                    "lastScreen": "0",
+                    "wallpaperplugin": "org.kde.image"
+                }
+            },
+            "height": 2.8333333333333335,
+            "hiding": "dodgewindows",
+            "location": "bottom",
+            "maximumLength": 85.33333333333333,
+            "minimumLength": 85.33333333333333,
+            "offset": 0
+        }
+    ],
+    "serializationFormatVersion": "1"
+}
+;
+
+plasma.loadSerializedLayout(layout);
